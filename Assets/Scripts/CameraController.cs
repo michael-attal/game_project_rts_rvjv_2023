d52b65f4b408152894bf4f3367cac99b224b165e
Chachoune963 <sacha@toutut.fr>
2023-12-18 18:56:42 +0100

    #3 Movements second draft
    Pretty smooth and stays at the same height. Still need to figure out better rotations.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,54 +1,49 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour
 {
-    public Camera camera;
     public float speed;
+    public float scrollSpeed;
+    public float maxYPosition;
+    public float minYPosition;
+    public float maxAngle;
+    public float minAngle;
 
-    private float angleAroundFocusPoint;
     private Vector3 movementInput;
-    private Vector2 mouseInput;
     private float scrollInput;
-    private bool lookingAround;
-
-    private float lookX = 0;
-    private float lookY = 0;
     
     void Update()
     {
         // Get basic movement inputs
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.z = Input.GetAxisRaw("Vertical");
-        movementInput.Normalize();
 
         scrollInput = Input.mouseScrollDelta.y;
-        
-        // Handle mouse for rotation inputs
-        if (Input.GetButton("Fire1"))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            lookX -= Input.GetAxisRaw("Mouse Y");
-            lookY += Input.GetAxisRaw("Mouse X");
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-        }
     }
 
     private void FixedUpdate()
     {
         Transform thisTransform = transform;
-        
-        // TODO: This isn't properly moving with rotations. Have to find a way to make movement inputs local somehow
-        Vector3 movement = movementInput * speed;
-        movement += thisTransform.forward * (scrollInput * speed);
 
-        thisTransform.Translate(movement * Time.fixedDeltaTime, Space.World);
-        transform.rotation = Quaternion.Euler(lookX, lookY, thisTransform.rotation.z);
+        Vector3 up = transform.worldToLocalMatrix.MultiplyVector(Vector3.up);
+        Vector3 movement = scrollInput * scrollSpeed * up + Vector3.ProjectOnPlane(movementInput, up).normalized * speed;
+        
+        thisTransform.Translate(movement * Time.fixedDeltaTime);
+
+        // Clamp the y position
+        var position = transform.position;
+        float yPos = Mathf.Clamp(position.y , minYPosition, maxYPosition);
+        transform.position = new Vector3(position.x, yPos, position.z);
+        
+        transform.rotation = Quaternion.Euler(GetCurrentAngle(), 0, thisTransform.rotation.z);
+    }
+
+    private float GetCurrentAngle()
+    {
+        float coef = (maxAngle - minAngle) / (maxYPosition - minYPosition);
+        float yPos = transform.position.y;
+        
+        return yPos * coef + minAngle - coef;
     }
 }
