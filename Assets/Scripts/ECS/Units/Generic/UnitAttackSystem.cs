b443428f77d7858f92470ecdf0dec3fc1638bc37
Chachoune963 <sacha@toutut.fr>
2024-02-11 19:58:59 +0100

    #15 [ADD] Units now attack and destroy nearest attackable object

--- Assets/Scripts/ECS/Units/Generic/UnitAttackSystem.cs
+++ Assets/Scripts/ECS/Units/Generic/UnitAttackSystem.cs
@@ -1,6 +1,8 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine;
 
 [UpdateBefore(typeof(TransformSystemGroup))]
 public partial struct UnitAttackSystem : ISystem
@@ -23,7 +25,39 @@ public partial struct UnitAttackSystem : ISystem
         if (!configManager.ActivateUnitAttackSystem)
         {
             state.Enabled = false;
-            // return;
+            return;
+        }
+        
+        foreach (var (attackerTransform, attackerInfo, attackerAttack) in SystemAPI.Query<RefRO<LocalTransform>, RefRO<Unit>, RefRW<UnitAttack>>().WithAll<UnitAttack>())
+        {
+            if (attackerAttack.ValueRO.CurrentReloadTime > 0f)
+            {
+                attackerAttack.ValueRW.CurrentReloadTime -= SystemAPI.Time.DeltaTime;
+                continue;
+            }
+            
+            float3 attackerPos = attackerTransform.ValueRO.Position;
+            RefRW<UnitDamage>? target = null;
+            float minimumRange = attackerAttack.ValueRO.Range;
+
+            foreach (var (attackableTransform, attackableInfo, attackableDamage) in SystemAPI.Query<RefRO<LocalTransform>, RefRO<Unit>, RefRW<UnitDamage>>().WithAll<UnitDamage>())
+            {
+                float3 attackablePos = attackableTransform.ValueRO.Position;
+
+                float currentDistance = attackerPos.DistanceTo(attackablePos);
+                if (currentDistance <= minimumRange && attackerInfo.ValueRO.SpeciesType != attackableInfo.ValueRO.SpeciesType)
+                {
+                    target = attackableDamage;
+                    minimumRange = currentDistance;
+                }
+            }
+
+            if (target.HasValue)
+            {
+                target.Value.ValueRW.Health -= attackerAttack.ValueRO.Strength;
+                attackerAttack.ValueRW.CurrentReloadTime = attackerAttack.ValueRO.RateOfFire;
+            }
+
         }
     }
 }
