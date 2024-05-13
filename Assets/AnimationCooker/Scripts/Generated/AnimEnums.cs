76b2e08a425beddbb7f788f9f6166078468098e5
Michaël ATTAL <michaelattal@icloud.com>
2024-05-13 21:34:41 +0200

    [ADD] Idle & Attack animations to Slime and prepare for futur others animations

---
+++ Assets/AnimationCooker/Scripts/Generated/AnimEnums.cs
@@ -0,0 +1,5 @@
+public enum Model : short { SlimeBasicUnitPrefab }
+public static class ModelSize { public const short Size = 1; }
+
+public enum SlimeBasicUnitPrefab : byte { Idle, Attack }
+public static class SlimeBasicUnitPrefabSize { public const byte Size = 2; }
