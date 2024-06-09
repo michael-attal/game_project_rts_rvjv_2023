978a14de6ed6400307af617ae0af3465b5524f0e
Michaël ATTAL <michaelattal@icloud.com>
2024-06-09 13:41:12 +0200

    [ADD] Activation setting for upgrade meca system
    [UPDATE] Upgrade Spawner to use initial scale and rotation
    [UPDATE] Rebase

--- Assets/Scripts/ECS/Units/Specialized/Meca/Upgrade/GlassCannonUpgradeSystem.cs
+++ Assets/Scripts/ECS/Units/Specialized/Meca/Upgrade/GlassCannonUpgradeSystem.cs
@@ -6,6 +6,7 @@ partial struct GlassCannonUpgradeSystem : ISystem
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
+        state.RequireForUpdate<Config>();
         state.RequireForUpdate<UnitAttack>();
         state.RequireForUpdate<UnitDamage>();
         state.RequireForUpdate<GlassCannonUpgrade>();
@@ -14,6 +15,17 @@ partial struct GlassCannonUpgradeSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        var configManager = SystemAPI.GetSingleton<Config>();
+
+        if (!configManager.ActivateMecaBasicUnitUpgradeSystem)
+        {
+            state.Enabled = false;
+            return;
+        }
+
+        if (configManager.IsGamePaused)
+            return;
+        
         var job = new GlassCannonUpgradeJob
         {
             ECB = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
