1a64ad23e60eec5911acdf67a014d2734c6350dd
Michaël ATTAL <michaelattal@icloud.com>
2024-06-11 13:02:27 +0200

    [FIX] Build with Conditional Field Property in Editor Folder
    [FIX] Build by removing unused shader file

--- Assets/Editor/ConditionalHideFieldPropertyEditor.cs
+++ Assets/Editor/ConditionalHideFieldPropertyEditor.cs
@@ -1,18 +1,6 @@
 using UnityEditor;
 using UnityEngine;
 
-public class ConditionalHideAttribute : PropertyAttribute
-{
-    public string ConditionalSourceField;
-    public int EnumValue;
-
-    public ConditionalHideAttribute(string conditionalSourceField, int enumValue)
-    {
-        ConditionalSourceField = conditionalSourceField;
-        EnumValue = enumValue;
-    }
-}
-
 [CustomPropertyDrawer(typeof(ConditionalHideAttribute))]
 public class ConditionalHidePropertyDrawer : PropertyDrawer
 {
