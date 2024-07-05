76c316c7fbe0687a898d5d327bd335ef0c841b86
Michaël ATTAL <michaelattal@icloud.com>
2024-07-05 18:03:40 +0200

    [ADD] Selection of species to play + difficulty slider + username
    [UPDATE] Deposit, Seek depot, Selection systems depending on the species selected
    [UPDATE] UI depending on the species selected and add Fusion UI
    [UPDATE] Prepare for the REST API score with username and score request

---
+++ Assets/Scripts/StartMenuPresenter.cs
@@ -0,0 +1,42 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuPresenter : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private TMP_Dropdown selectionRaceDropdown;
+    [SerializeField] private Slider difficultySlider;
+    [SerializeField] private TMP_InputField playerNameInputField;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Loading battlefield scene...");
+
+        var selectedRaceIndex = selectionRaceDropdown.value;
+        var selectedRace = selectionRaceDropdown.options[selectedRaceIndex].text;
+        Debug.Log($"Selected Race Index: {selectedRaceIndex}");
+        Debug.Log($"Selected Race: {selectedRace}");
+
+        var difficultyLevel = difficultySlider.value;
+        Debug.Log($"Difficulty Level: {difficultyLevel}");
+
+        var playerName = playerNameInputField.text;
+        Debug.Log($"Player Name: {playerName}");
+
+        var gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.SelectedRace = selectedRace;
+        gameManager.DifficultyLevel = difficultyLevel;
+        gameManager.PlayerName = playerName; // Stocker le nom du joueur
+
+        // Changer de scène
+        var sceneLoader = gameManager.GetComponent<SceneLoader>();
+        sceneLoader.sceneToLoad = Scenes.BattlefieldScene;
+        StartCoroutine(sceneLoader.LoadSceneAsync());
+    }
+}
